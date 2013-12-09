1bab5c29d5debeb3b9f5bd576744cba0511b7ab8
Benjamin Mosior <bemosior@ship.edu>
2013-12-09 10:36:21 -0500

    Added logic to check if get a campaign if it is active and relevant to the user's group memberships.

--- app/Controller/Component/CampaignManagerComponent.php
+++ app/Controller/Component/CampaignManagerComponent.php
@@ -2,25 +2,72 @@
 App::uses('Component', 'Controller');
 class CampaignManagerComponent extends Component {
 
-	/**
-	 * Determines if a user's identifier/memberships require them to complete a campaign.
-	 */
-    public function hasCampaigns($identifier, $memberships) {
+    public $components = array('CampaignManager');
+    
+    /**
+     * Determines if a user's identifier/memberships require them to complete a campaign.
+     */
+    public function userHasCampaigns($identifier, $memberships) {
 
-        //Load the local user roles defined for comparison
+        //Load the local user roles model for comparison
         $userRole = ClassRegistry::init('UserRole');
 
         //Find all of the user's roles that are defined.
         $relevantRoles = $userRole->find('all', array(
-            'conditions' => array('UserRole.external_name' => $memberships)
+            'conditions' => array('UserRole.external_name' => $memberships),
+            'fields'=>array('id','local_name')
         ));
 
         error_log('IDENTIFIER: ' . $identifier . ', MEMBERSHIPS: ' . count($relevantRoles));
+        
+        $memberships = array();
         foreach ($relevantRoles as $role) {
             error_log("MEMBERSHIP: " . $role['UserRole']['local_name']);
+            array_push($memberships, $role['UserRole']['id']);
         }
 
-        if ($identifier == "someidentifier") {
+        if ($this->roleHasCampaigns($memberships)) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+
+    /**
+     * Determines if memberships have outstanding campaigns.
+     */
+    public function roleHasCampaigns($memberships) {
+
+        //Load the Campaigns model
+        $campaigns = ClassRegistry::init('Campaigns');
+
+        //Complicated join to support HABTM nature of the Campaigns-UserRoles models.
+        //Checks for active campaigns applicable to the user's memberships
+        $availableCampaigns = $campaigns->find('first', array(
+            'joins' => array(
+               array('table' => 'campaigns_user_roles',
+                   'alias' => 'CampaignUserRoles',
+                   'type' => 'inner',
+                   'conditions' => array(
+                       'Campaigns.id = CampaignUserRoles.campaign_id'
+                   )
+               ),
+               array('table' => 'user_roles',
+                   'alias' => 'UserRoles',
+                   'type' => 'inner',
+                   'conditions' => array(
+                       'CampaignUserRoles.user_role_id = UserRoles.id'
+                   )
+               )
+            ),        
+            'conditions' => array(
+                'Campaigns.active' => true,
+                'UserRoles.id' => $memberships,
+            ),
+        ));
+
+        if(count($availableCampaigns) > 0) {
             return 1;
         }
 
